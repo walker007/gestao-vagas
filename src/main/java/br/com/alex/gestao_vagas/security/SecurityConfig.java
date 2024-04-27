9f118ed53bddacfe933fb251397af4c7afa109de
Alex Xavier <alexjr.123@hotmail.com>
2024-04-27 17:07:51 -0300

    fix: Remove Bean de Gerenciamento de usuário em Memória

--- src/main/java/br/com/alex/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/alex/gestao_vagas/security/SecurityConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -34,11 +32,6 @@ public class SecurityConfig {
                 ).addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
         ;
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager();
     }
 
     @Bean
